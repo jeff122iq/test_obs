2aab988bec66594a6bce14cc7af3dbbdbbc05c03
jeff122iq <jeff122iq@gmail.com>
2021-07-27 10:36:36 +0300

    Sent test task to Rodion

--- server/controllers/athletController.js
+++ server/controllers/athletController.js
@@ -11,7 +11,8 @@ exports.getAllAthletes = (req, res) => {
   try {
     let sql = `SELECT DISTINCT Game.game_id, year, city, A.athlete_id, name
                FROM Game
-                   LEFT JOIN AthleteResult AR on Game.game_id
+                   LEFT JOIN AthleteResult AR
+               on Game.game_id
                    LEFT JOIN Athlete A on A.athlete_id = AR.game_id
                ORDER BY year, city, name;
     `;
@@ -22,15 +23,18 @@ exports.getAllAthletes = (req, res) => {
         throw err;
       }
       const response = rows.reduce((acc, row) => {
-        const names = rows.reduce((accum, curr) => {
-          if (curr.year === row.year && curr.city === row.city ) accum.push({athlete_id: curr.athlete_id, name: curr.name});
+        const athletes = rows.reduce((accum, curr) => {
+          if (curr.year === row.year && curr.city === row.city) accum.push({
+            athlete_id: curr.athlete_id,
+            name: curr.name
+          });
           return accum
         }, [])
         if (!acc.find((el) => el.city === row.city)) {
           acc.push({
             city: row.city,
             year: row.year,
-            names
+            athletes
           })
         }
 
@@ -44,5 +48,42 @@ exports.getAllAthletes = (req, res) => {
 }
 
 exports.currentAthlete = (req, res) => {
+  try {
+    const response = () => {
+      const id = 1
+      let sql = `SELECT *
+                 FROM Athlete
+                          INNER JOIN AthleteResult AR on Athlete.athlete_id = AR.athlete_id
+                          INNER JOIN Game G on AR.game_id = G.game_id
+                 WHERE Athlete.athlete_id = ${id};
+      `;
 
+    }
+    db.all(sql, [], (err, rows) => {
+      if (err) {
+        throw err;
+      }
+      const response = rows.reduce((acc, row) => {
+        const athletes = rows.reduce((accum, curr) => {
+          if (curr.year === row.year && curr.city === row.city) accum.push({
+            athlete_id: curr.athlete_id,
+            name: curr.name
+          });
+          return accum
+        }, [])
+        if (!acc.find((el) => el.city === row.city)) {
+          acc.push({
+            city: row.city,
+            year: row.year,
+            athletes
+          })
+        }
+
+        return acc;
+      }, []);
+      res.status(200).json({response})
+    });
+  } catch (e) {
+
+  }
 }
